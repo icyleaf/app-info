0839c13ff9ea6cc134a50a5257521a70d4e7f355
icyleaf <icyleaf.cn@gmail.com>
2019-06-17 15:10:21 +0800

    feat(android): add size, use_permissions, signs, certificates, activities, services components and rename uses_features to use_features

--- app_info.gemspec
+++ app_info.gemspec
@@ -22,7 +22,7 @@ Gem::Specification.new do |spec|
   spec.add_dependency 'ruby_android', '~> 0.7.7'
   spec.add_dependency 'image_size', '~> 1.5.0'
 
-  spec.add_development_dependency 'bundler', '~> 1.12'
-  spec.add_development_dependency 'rake', '~> 10.0'
+  spec.add_development_dependency 'bundler', '>= 1.12'
+  spec.add_development_dependency 'rake', '>= 10.0'
   spec.add_development_dependency 'rspec', '~> 3.0'
 end
--- spec/app_info/parser/apk_spec.rb
+++ spec/app_info/parser/apk_spec.rb
@@ -3,6 +3,8 @@ describe AppInfo::Parser::APK do
     let(:file) { File.dirname(__FILE__) + '/../../fixtures/apps/android.apk' }
     subject { AppInfo::Parser::APK.new(file) }
 
+    it { expect(subject.size).to eq(3070618) }
+    it { expect(subject.size(humanable: true)).to eq('2.93 MB') }
     it { expect(subject.os).to eq 'Android' }
     it { expect(subject.wear?).to be false }
     it { expect(subject.tv?).to be false }
@@ -16,9 +18,19 @@ describe AppInfo::Parser::APK do
     it { expect(subject.identifier).to eq('com.icyleaf.appinfodemo') }
     it { expect(subject.icons.length).not_to be_nil }
     it { expect(subject.min_sdk_version).to eq 14 }
-    it { expect(subject.target_sdk_version).to eq 23 }
+    it { expect(subject.target_sdk_version).to eq 29 }
+    it { expect(subject.certificates.first).to be_kind_of(AppInfo::Parser::APK::Certificate) }
+    it { expect(subject.certificates.first.path).to eq('META-INF/CERT.RSA') }
+    it { expect(subject.certificates.first.certificate).to be_kind_of(OpenSSL::X509::Certificate) }
+    it { expect(subject.signs.first).to be_kind_of(AppInfo::Parser::APK::Sign) }
+    it { expect(subject.signs.first.path).to eq('META-INF/CERT.RSA') }
+    it { expect(subject.signs.first.sign).to be_kind_of(OpenSSL::PKCS7) }
+    it { expect(subject.activities.size).to eq(1) }
+    it { expect(subject.services.size).to eq(0) }
+    it { expect(subject.components.size).to eq(1) }
+    it { expect(subject.use_permissions.first).to eq('android.permission.BLUETOOTH') }
     it { expect(subject.manifest).to be_kind_of Android::Manifest }
-    it { expect(subject.manifest.use_permissions.first).to eq('android.permission.INTERNET') }
+    it { expect(subject.manifest.use_permissions).to eq(subject.use_permissions) }
   end
 
   describe '#Wear' do
