1eef281e2e229fdff5be72df7268e4c40c3442e7
icyleaf <icyleaf.cn@gmail.com>
2022-11-08 23:03:35 +0800

    fix: mktmpdir

--- lib/app_info/helper.rb
+++ lib/app_info/helper.rb
@@ -85,7 +85,7 @@ module AppInfo
       def tempdir(file, prefix:)
         dest_path ||= File.join(File.dirname(file), prefix)
         dest_file = File.join(dest_path, File.basename(file))
-        Dir.mkdir(dest_path, 0_700)
+        FileUtils.mkdir_p(dest_path, mode: 0_700)
         dest_file
       end
     end
